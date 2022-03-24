e042a413ce2f4335c3f6dbdd4992ad6bbec95cf9
Matome Moagi <MMoagi2@sars.gov.za>
2022-03-24 21:47:12 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Borderless Recruitment</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Free Website Template" name="keywords">
+        <meta content="Free Website Template" name="description">
+
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="icon">
+
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"> 
+        
+        <!-- CSS Libraries -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <!-- Top Bar Start -->
+        <div class="top-bar">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-4 col-md-12">
+                        <div class="logo">
+                            <a href="index.html">
+                                <img src="img/logo.jpg" alt="Logo" width="40%" height="100%">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-8 col-md-7 d-none d-lg-block">
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="top-bar-item">
+                                    <div class="top-bar-icon">
+                                        <i class="far fa-clock"></i>
+                                    </div>
+                                    <div class="top-bar-text">
+                                        <h3>Opening Hours</h3>
+                                        <p>Mon-Fri, 8:00AM - 5:00PM</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="top-bar-item">
+                                    <div class="top-bar-icon">
+                                        <i class="fa fa-phone-alt"></i>
+                                    </div>
+                                    <div class="top-bar-text">
+                                        <h3>Call Us</h3>
+                                        <p>012-383-2136</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <div class="top-bar-item">
+                                    <div class="top-bar-icon">
+                                        <i class="far fa-envelope"></i>
+                                    </div>
+                                    <div class="top-bar-text">
+                                        <h3>Email Us</h3>
+                                        <p>info@borderlessrecruitment.co.za</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Top Bar End -->
+
+        <!-- Nav Bar Start -->
+        <div class="nav-bar">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+                    <a href="#" class="navbar-brand">MENU</a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav mr-auto">
+                            <a href="index.html" class="nav-item nav-link active">Home</a>                            
+                        </div>
+                        <div class="ml-auto">
+                            
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <!-- Nav Bar End -->
+
+
+        <!-- Carousel Start -->
+        <div class="carousel">
+            <div class="container-fluid">
+                <div class="owl-carousel">
+                    <div class="carousel-item">
+                        <div class="carousel-img">
+                            <img src="img/main-burner.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-text">
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="carousel-img">
+                            <img src="img/slide-two.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-text">
+                            <h3>Borderless Recruitment</h3>
+                            <h1>Coming Soon...</h1>
+                            <p>
+                                Your Career is our business... 
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="carousel-img">
+                            <img src="img/slide-three.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-text">
+                            <h3>Student Services</h3>
+                            <h1>Coming Soon...</h1>
+                            <p>
+                                Your Career is our business...
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="carousel-img">
+                            <img src="img/slide-four.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-text">
+                            <h3>Job Seeker Services</h3>
+                            <h1>Coming Soon...</h1>
+                            <p>
+                                Your Career is our business...
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="carousel-img">
+                            <img src="img/slide-one.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-text">
+                            <h3>Employer Services</h3>
+                            <h1>Coming Soon...</h1>
+                            <p>
+                                Your Career is our business...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Carousel End -->
+        
+
+        <!-- About Start -->
+        <div class="about">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="about-img">
+                            <img src="img/about2.jpg" alt="Image">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="section-header text-left">
+                            <p>About Us</p>
+                            <h3><strong>Boarderless Recruitment Services</strong></h3>
+                        </div>
+                        <div class="about-content">
+                            <p>
+                                <strong>Borderless Recruitment for Students</strong> <br /> 
+                                Borderless Recruitment started its journey back in 2013 as a company that was recruiting foreign students who wanted to study in South Africa. The company initially 
+                                focused on the students within the Southern African Development Community (SADC) region where recruitment was done for IIEMSA (formerly known as Monash South Africa) 
+                                and Inscape Education Group at the time, both South African based institutions of higher learning. 
+                                <br /><br />
+                                As Borderless Recruitment grows by leaps and bounds we have expanded our student recruitment services to the universities in Europe, Middle East and North America. 
+                            </p>
+                            <p>
+                                <strong>Mission</strong><br />
+                                To provide useful and accurate information and guidance to students in order to make sound career choices. 
+                                <br /><br />
+                                <strong>Values</strong><br />
+                                We treat each other, our clients and students with mutual respect and sensitivity. <br />
+                                We provide encouragement and emotional support for our students 
+                            </p>
+                            <p><strong>Our Services</strong></p>
+                            <ul>
+                                <li><i class="far fa-check-circle"></i>Student Services</li>
+                                <li><i class="far fa-check-circle"></i>Job Seeker Services</li>
+                                <li><i class="far fa-check-circle"></i>Employer Services</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+
+        <!-- Footer Start -->
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-contact">
+                            <h2>Get In Touch</h2>
+                            <p><i class="fa fa-calendar"></i>Monday - Friday: 8:30 AM to 5:00 PM</p>
+                            <p><i class="fa fa-map-marker-alt"></i>Pretoria, Gauteng, RSA, 0002</p>
+                            <p><i class="fa fa-phone-alt"></i>+27 (012) 383-2136</p>
+                            <p><i class="fa fa-envelope"></i>info@borderlessrecruitment.co.za</p>
+                            <div class="footer-social">
+                                <a class="btn" href="#"><i class="fab fa-twitter"></i></a>
+                                <a class="btn" href="https://www.facebook.com/borderlessrecruitment"><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn" href="#"><i class="fab fa-youtube"></i></a>
+                                <a class="btn" href="#"><i class="fab fa-instagram"></i></a>
+                                <a class="btn" href="#"><i class="fab fa-linkedin-in"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-link">
+                            <h2>Popular Links</h2>
+                            <a href="#">About Us</a>
+                            <a href="#">Contact Us</a>
+                            <a href="#">Our Service</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="footer-link">
+                            <h2>Useful Links</h2>
+                            <a href="#">Terms of use</a>
+                            <a href="#">Privacy policy</a>
+                            <a href="#">Cookies</a>
+                            <a href="#">Help</a>
+                            <a href="#">FQAs</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container copyright">
+                <p>&copy; <a href="#">www.borderlessrecruitment.co.za</a>, All Right Reserved. Designed By <a href="https://matomemoagishai.co.za">Matome Moagi Shai</a></p>
+            </div>
+        </div>
+        <!-- Footer End -->
+        
+        <!-- Back to top button -->
+        <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+        
+        <!-- Pre Loader -->
+        <div id="loader" class="show">
+            <div class="loader"></div>
+        </div>
+
+        <!-- JavaScript Libraries -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/counterup/counterup.min.js"></script>
+        
+        <!-- Contact Javascript File -->
+        <script src="mail/jqBootstrapValidation.min.js"></script>
+        <script src="mail/contact.js"></script>
+
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
